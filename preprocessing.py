d21dde3f479d4edca414a86babbcb1ed111fcf68
Khadijah Olufayo <khadijaholufayo@college.harvard.edu>
2026-04-29 21:11:53 -0400

    please try running preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,7 +1,13 @@
 import networkx as nx
-#import pickle
+import pickle
 
 G = nx.DiGraph()
+
+#FOR TEST
+#with open("graph.pkl", "rb") as f:
+    #G2 = pickle.load(f)
+
+#print("here", G2.number_of_nodes(), G2.number_of_edges())
 
 with open("top-categories/wiki-topcats-page-names.txt", 'r', encoding="utf-8") as f:
     for line in f:
@@ -24,5 +30,11 @@ with open("top-categories/wiki-topcats.txt", 'r', encoding="utf-8") as f:
         u, v = map(int, line.split())
         G.add_edge(u,v)
 
+
 print("Nodes: ", G.number_of_nodes())
 print("Edges: ", G.number_of_edges())
+
+with open("graph.pkl", "wb") as f:
+    pickle.dump(G, f)
+
+print(".pickl is done done")
